c4af4abcfc11b1bb9359f9d12263a355e90ac04e
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-11-04 19:11:56 +0500

    Finished timer and quarter buttin stretches

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, setLionsScore] = useState(20);
   const [tigersScore, setTigersScore] = useState(17);
+  const [quarter, setQuarter] = useState(1);
+  const [time, setTime] = useState(910);
   function touchDown(scoreSetter, teamScore){
     scoreSetter(teamScore+7);
   }
   function fieldGoal(scoreSetter, teamScore){
     scoreSetter(teamScore+3);
   }
-  
+  React.useEffect(() => {
+    let id = setInterval(() => {
+      setTime(time - 1);
+    }, 1000);
+    if(time<=2700) setQuarter(2);
+    if(time<=1800) setQuarter(3);
+    if(time<=900) setQuarter(4);
+    
+    return ()=> clearInterval(id);
+  });
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -26,13 +38,13 @@ function App() {
 
             <div className="home__score">{lionsScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{time>=600 ? Math.floor(time/60) : (`0`+Math.floor(time/60))}:{time%60>=10 ? time%60: (`0`+time%60)}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{tigersScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -52,6 +64,9 @@ function App() {
             e=>fieldGoal(setTigersScore, tigersScore)
           }>Away Field Goal</button>
         </div>
+        <button className = 'quarterButton' onClick = {e => setQuarter(quarter+1)}>
+          Next quarter
+        </button>
       </section>
     </div>
   );
